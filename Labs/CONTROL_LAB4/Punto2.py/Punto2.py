a7620ca70e8aa95ff3233cbadac1957d9a4e9334
jmmelendez <mateomelendezvilladiego12@gmail.com>
2023-10-27 08:25:31 -0500

    Punto 1

--- Labs/CONTROL_LAB4/Punto2.py/Punto2.py
+++ Labs/CONTROL_LAB4/Punto2.py/Punto2.py
@@ -4,12 +4,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 # Título de la aplicación
+# Título de la aplicación
 st.title("Análisis de respuesta en frecuencia")
 
 st.write("Esta aplicación permite encontrar la función de transferencia de un sistema de control a partir del análisis de su respuesta en frecuencia.")
 
 # Botón para cargar el archivo CSV
 uploaded_file = st.file_uploader("Cargar un archivo CSV", type=["csv"])
+
+# Selector para el tipo de frecuencia (Hz o Rad/s)
+freq_type = st.selectbox("Unidades de frecuencia", ["Hz", "Rad/s"])
 
 if uploaded_file is not None:
     # Lee el archivo CSV en un DataFrame de pandas
@@ -20,23 +24,27 @@ if uploaded_file is not None:
         Mag = df['Mag'].ravel().tolist()
         Phase = df['Phase'].ravel().tolist()
         Frec = df['W'].ravel().tolist()
-        Hz= np.rad2deg(Frec)
 
+        # Convierte las frecuencias a radianes por segundo si es necesario
+        if freq_type == "Hz":
+            Frec = np.deg2rad(Frec)
 
         # Crear la figura para magnitud
         fig_mag, ax_mag = plt.subplots()
         ax_mag.semilogx(Frec, Mag)
         ax_mag.set_ylabel('Magnitud')
-        ax_mag.set_xlabel('Frecuencia (Hz)')
+        ax_mag.set_xlabel(f'Frecuencia ({freq_type})')
         ax_mag.set_title('Respuesta del sistema experimental (Magnitud)')
 
         # Crear la figura para fase
         fig_phase, ax_phase = plt.subplots()
         ax_phase.semilogx(Frec, Phase)
-        ax_phase.set_xlabel('Frecuencia (Hz)')
+        ax_phase.set_xlabel(f'Frecuencia ({freq_type})')
         ax_phase.set_ylabel('Fase (grados)')
         ax_phase.set_title('Respuesta del sistema experimental (Fase)')
 
         # Mostrar la gráfica en Streamlit
-        st.pyplot(fig_mag)
-        st.pyplot(fig_phase)
+        if st.button("Graficar"):
+            # Mostrar la gráfica en Streamlit
+            st.pyplot(fig_mag)
+            st.pyplot(fig_phase)
