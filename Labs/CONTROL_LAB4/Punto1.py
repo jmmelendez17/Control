81339483751dc7f0033912734e6770324bd8d59a
jmmelendez <mateomelendezvilladiego12@gmail.com>
2023-10-28 20:17:34 -0500

    Punto 2

--- Labs/CONTROL_LAB4/Punto1.py
+++ Labs/CONTROL_LAB4/Punto1.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 import control as ct
 
 # Definir las funciones de transferencia de los sistemas
-G0 = ct.TransferFunction([20], [1])  # Cero 1
+G0 = ct.TransferFunction([20/50], [1])  # Cero 1
 G1 = ct.TransferFunction([1, 1], [1])  # Cero 1
 G2 = ct.TransferFunction([1], [1, 0])     # Polo en origen
-G3 = ct.TransferFunction([1], [1, 5])     # Polo en 5
-G4 = ct.TransferFunction([1], [1, 2, 10])     # Polo cuadrático
+G3 = ct.TransferFunction([1], [1/5, 1])     # Polo en 5
+G4 = ct.TransferFunction([1], [1/10, 2/10, 1])     # Polo cuadrático
 
 # Crear una lista de frecuencias en un rango más amplio
 frequencies = np.logspace(-1, 3, 1000)
@@ -18,36 +18,52 @@ w1, mag1, phase1 = ct.bode_plot(G1, omega=frequencies, dB=True, label='Cero 1')
 w2, mag2, phase2 = ct.bode_plot(G2, omega=frequencies, dB=True, label='Polo en origen', linestyle='dashed')
 w3, mag3, phase3 = ct.bode_plot(G3, omega=frequencies, dB=True, label='Polo en 5', linestyle='dashed')
 w4, mag4, phase4 = ct.bode_plot(G4, omega=frequencies, dB=True, label='Polo cuadrático')
-plt.xlabel('Frecuencia (rad/s)')
-plt.ylabel('Fase (grados)')
+plt.xlabel('Frequency (rad/s)')
 plt.grid(True)
 
-# Agregar leyenda
-plt.legend()
 
 # Personalizar etiquetas y título
-plt.suptitle('Bosquejo de diagrama de Bode')
+plt.suptitle('Bosquejo de diagrama de Bode', fontsize=28, fontweight='bold')
 plt.tight_layout()
 plt.subplots_adjust(top=0.88)
 # Personalizar etiquetas de los ejes
-plt.xlabel('Frecuencia (rad/s)')
+plt.xlabel('Frequency (rad/s)')
 plt.subplots_adjust(hspace=0.4)
 
 plt.show()
 
 # Definir las funciones de transferencia de los sistemas
 Gs = ct.TransferFunction([20,20], [1, 7, 20, 50, 0])  #función completa
-ws, mags, phases = ct.bode_plot(Gs, omega=frequencies, dB=True, color='purple', margins=True)
+ws, mags, phases = ct.bode_plot(Gs, omega=frequencies, dB=True, color='purple')
 Margin1= ct.margin(Gs)
+print(Margin1)
+time, response = ct.step_response(Gs)
+error_position = 1 - response[-1]  # Error de posición
 
+# Para calcular el error de velocidad, necesitas encontrar la derivada de la respuesta
+# Aquí, se utiliza un enfoque de diferencia finita para aproximar la derivada
+time_diff = np.diff(time)
+response_diff = np.diff(response)
+error_velocity = response_diff[-1] / time_diff[-1]  # Error de velocidad
+
+time_diff2 = np.diff(time_diff)
+velocity_diff = np.diff(response_diff)
+error_acceleration = velocity_diff[-1] / time_diff2[-1]  # Error de aceleración
+
+print("Error de posición:", error_position)
+print("Error de velocidad:", error_velocity)
+print("Error de aceleración:", error_acceleration)
+plt.grid(True)
 
 # Personalizar etiquetas y título
-plt.suptitle('Diagrama de Bode')
+plt.suptitle('Diagrama de Bode', fontsize=28, fontweight='bold')
 plt.tight_layout()
 plt.subplots_adjust(top=0.88)
 # Personalizar etiquetas de los ejes
-plt.xlabel('Frecuencia (rad/s)')
+plt.xlabel('Frequency (rad/s)')
 plt.subplots_adjust(hspace=0.4)
+
+
 
 plt.show()
 
@@ -55,11 +71,11 @@ plt.show()
 #Gráfica para K=40
 
 # Definir las funciones de transferencia de los sistemas
-G0 = ct.TransferFunction([40], [1])  # Cero 1
+G0 = ct.TransferFunction([40/50], [1])  # Cero 1
 G1 = ct.TransferFunction([1, 1], [1])  # Cero 1
 G2 = ct.TransferFunction([1], [1, 0])     # Polo en origen
-G3 = ct.TransferFunction([1], [1, 5])     # Polo en 5
-G4 = ct.TransferFunction([1], [1, 2, 10])     # Polo cuadrático
+G3 = ct.TransferFunction([1], [1/5, 1])     # Polo en 5
+G4 = ct.TransferFunction([1], [1/10, 2/10, 1])     # Polo cuadrático
 
 # Crear una lista de frecuencias en un rango más amplio
 frequencies = np.logspace(-1, 3, 1000)
@@ -70,37 +86,52 @@ w1, mag1, phase1 = ct.bode_plot(G1, omega=frequencies, dB=True, label='Cero 1')
 w2, mag2, phase2 = ct.bode_plot(G2, omega=frequencies, dB=True, label='Polo en origen', linestyle='dashed')
 w3, mag3, phase3 = ct.bode_plot(G3, omega=frequencies, dB=True, label='Polo en 5', linestyle='dashed')
 w4, mag4, phase4 = ct.bode_plot(G4, omega=frequencies, dB=True, label='Polo cuadrático')
-plt.xlabel('Frecuencia (rad/s)')
-plt.ylabel('Fase (grados)')
+plt.xlabel('Frequency (rad/s)')
 plt.grid(True)
 
-# Agregar leyenda
-plt.legend()
 
 # Personalizar etiquetas y título
-plt.suptitle('Bosquejo de diagrama de Bode')
+plt.suptitle('Bosquejo de diagrama de Bode', fontsize=28, fontweight='bold')
 plt.tight_layout()
 plt.subplots_adjust(top=0.88)
 # Personalizar etiquetas de los ejes
-plt.xlabel('Frecuencia (rad/s)')
 plt.subplots_adjust(hspace=0.4)
 
 plt.show()
 
 # Definir las funciones de transferencia de los sistemas
 Gs = ct.TransferFunction([40,40], [1, 7, 20, 50, 0])  #función completa
-ws, mags, phases = ct.bode_plot(Gs, omega=frequencies, dB=True, color='purple', margins=True)
+ws, mags, phases = ct.bode_plot(Gs, omega=frequencies, dB=True, color='purple')
 Margin2= ct.margin(Gs)
+print(Margin2)
+time, response = ct.step_response(Gs)
+error_position = 1 - response[-1]  # Error de posición
 
+# Para calcular el error de velocidad, necesitas encontrar la derivada de la respuesta
+# Aquí, se utiliza un enfoque de diferencia finita para aproximar la derivada
+time_diff = np.diff(time)
+response_diff = np.diff(response)
+error_velocity = response_diff[-1] / time_diff[-1]  # Error de velocidad
+
+time_diff2 = np.diff(time_diff)
+velocity_diff = np.diff(response_diff)
+error_acceleration = velocity_diff[-1] / time_diff2[-1]  # Error de aceleración
+
+print("Error de posición:", error_position)
+print("Error de velocidad:", error_velocity)
+print("Error de aceleración:", error_acceleration)
 # Personalizar etiquetas y título
-plt.suptitle('Diagrama de Bode')
+plt.grid(True)
+plt.suptitle('Diagrama de Bode', fontsize=28, fontweight='bold')
 plt.tight_layout()
 plt.subplots_adjust(top=0.88)
 # Personalizar etiquetas de los ejes
-plt.xlabel('Frecuencia (rad/s)')
 plt.subplots_adjust(hspace=0.4)
-
 plt.show()
 
-
-
+ct.pzmap(Gs)
+plt.title('Mapa de polos y ceros', fontsize=24, fontweight='bold')
+plt.xlabel('Reales')
+plt.ylabel('Imaginarios')
+plt.grid(True)
+plt.show()
--- Labs/CONTROL_LAB4/Punto2.py/Punto2.py
+++ Labs/CONTROL_LAB4/Punto2.py/Punto2.py
@@ -3,17 +3,15 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import control as ct
 # Título de la aplicación
 # Título de la aplicación
 st.title("Análisis de respuesta en frecuencia")
 
-st.write("Esta aplicación permite encontrar la función de transferencia de un sistema de control a partir del análisis de su respuesta en frecuencia.")
+st.write("Esta aplicación permite encontrar las características de la respuesta en frecuencia de un sistema de control a partir de su caracterización.")
 
 # Botón para cargar el archivo CSV
 uploaded_file = st.file_uploader("Cargar un archivo CSV", type=["csv"])
-
-# Selector para el tipo de frecuencia (Hz o Rad/s)
-freq_type = st.selectbox("Unidades de frecuencia", ["Hz", "Rad/s"])
 
 if uploaded_file is not None:
     # Lee el archivo CSV en un DataFrame de pandas
@@ -21,30 +19,120 @@ if uploaded_file is not None:
 
     # Haz lo que quieras con el DataFrame (por ejemplo, mostrarlo)
     if df is not None:
-        Mag = df['Mag'].ravel().tolist()
+        Mag = ct.mag2db(df['Mag'].ravel().tolist())
         Phase = df['Phase'].ravel().tolist()
         Frec = df['W'].ravel().tolist()
 
-        # Convierte las frecuencias a radianes por segundo si es necesario
-        if freq_type == "Hz":
-            Frec = np.deg2rad(Frec)
+        #frecuencia y pico de resonancia
+        maxi=np.max(Mag)
+        prueba=maxi - Mag[0] #Mira si hay resonancia
+        if prueba > 0.2:
+            indice_del_pico = np.argmax(Mag)
+            frecuencia_del_pico = round(Frec[indice_del_pico], 3)
+            Pico=round(maxi, 3)
 
+        #Margen de ganancia 
+        # Inicializar variables para almacenar la frecuencia y la magnitud correspondiente
+        frecuencia_con_fase_minus_180 = None
+        magnitud_en_frecuencia = None
+
+        for i, fase in enumerate(Phase):
+            if abs(fase + 180) == 0 :
+                frecuencia_con_fase_minus_180 = Frec[i]
+                magnitud_en_frecuencia = Mag[i]
+                break
+        
+        if magnitud_en_frecuencia is not None:
+            gainmar=round(0-magnitud_en_frecuencia, 3)
+        else:
+            gainmar= None
+
+        #Margen de fase 
+        # Inicializar variables para almacenar la frecuencia y la fase correspondiente
+        frecuencia_con_mag_1 = None
+        Fase_en_frec= None
+
+        for i, magnitud in enumerate(Mag):
+            if magnitud <= 0:
+                frecuencia_con_mag_1 = round(Frec[i], 3)
+                Fase_en_frec= round(Phase[i], 3)
+                break
+        
+        phasenmar=round(180+Fase_en_frec,3)
+
+        ##Constantes de error
+        Kp = 1 / Mag[0]  # Error de posición
+        Kv = 1 / (Mag[0] * Frec[0])  # Error de velocidad
+        Ka = 1 / (Mag[0] * Frec[0]** 2)  # Error de aceleración
+        print(Kp)
+        print(Kv)
+        print(Ka)
         # Crear la figura para magnitud
         fig_mag, ax_mag = plt.subplots()
-        ax_mag.semilogx(Frec, Mag)
-        ax_mag.set_ylabel('Magnitud')
-        ax_mag.set_xlabel(f'Frecuencia ({freq_type})')
+        ax_mag.semilogx(Frec, Mag, color='purple', label='Magnitud del sistema')
+        ax_mag.set_ylabel('Magnitud (dB)')
+        ax_mag.set_xlabel('frecuencia (rad/s)')
         ax_mag.set_title('Respuesta del sistema experimental (Magnitud)')
+        if frecuencia_del_pico is not None:
+            ax_mag.vlines(x=frecuencia_del_pico, ymin=Mag[len(Mag)-1], ymax=Pico, linestyles='dashed', color='red', label='Frecuencia de resonancia')
+        if frecuencia_con_mag_1 is not None:
+            ax_mag.hlines(y=0, xmin=0, xmax=frecuencia_con_mag_1- 0.02, linestyles='dashed', color='Blue', label='Ancho de banda')
+        if  frecuencia_con_fase_minus_180 is not None:
+            ax_mag.vlines(x=frecuencia_con_fase_minus_180, ymin=0, ymax=magnitud_en_frecuencia, linestyles='dashed', color='Green', label='Margen de ganancia')
+        ax_mag.legend()
+        ax_mag.grid(which='both', linestyle='--')
 
         # Crear la figura para fase
         fig_phase, ax_phase = plt.subplots()
-        ax_phase.semilogx(Frec, Phase)
-        ax_phase.set_xlabel(f'Frecuencia ({freq_type})')
+        ax_phase.semilogx(Frec, Phase, color='Purple', label='Fase del sistema')
+        ax_phase.set_xlabel('frecuencia (rad/s)')
         ax_phase.set_ylabel('Fase (grados)')
         ax_phase.set_title('Respuesta del sistema experimental (Fase)')
+        if frecuencia_con_mag_1 is not None:
+            ax_phase.vlines(x=frecuencia_con_mag_1, ymin=-180, ymax=Fase_en_frec, linestyles='dashed', color='Green', label='Margen de fase')
+        ax_phase.legend()
+        ax_phase.grid(which='both', linestyle='--')
+
 
         # Mostrar la gráfica en Streamlit
         if st.button("Graficar"):
             # Mostrar la gráfica en Streamlit
             st.pyplot(fig_mag)
             st.pyplot(fig_phase)
+
+            st.write(f"El ancho de banda del sistema es de {frecuencia_con_mag_1} rad/s")
+            if prueba > 0.2:
+                st.write(f"Existe un pico de resonancia de {Pico} dB y se encuentra en {frecuencia_del_pico} rad/s:")
+            else:
+                st.write("No Existe un picode resonancia")
+
+            st. write(f"El margen de fase del sistema es de {phasenmar} grados")
+
+            if gainmar is not None:
+                st. write(f"El margen de ganancia del sistema es de {gainmar} dB")
+            else:
+                st.write("el margen de ganancia es infinito")
+            
+            if Kp>5:
+                st.write("La constante de error estático de posición es aproximadamente infinito")
+            else:
+                if Kp< 0.01:
+                    st.write("La constante de error estático de posición es aproximadamente 0")
+                else:
+                    st.write(f"la constante de error estática de posición es {round(Kp, 3)}")
+            if Kv>5:
+                st.write("La constante de error estático de velocidad es aproximadamente infinito")
+            else:
+                if Kv< 0.01:
+                    st.write("La constante de error estático de velocidad es aproximadamente 0")
+                else:
+                    st.write(f"la constante de error estática de velocidad es {round(Kp, 3)}")
+            if Ka>5:
+                st.write("La constante de error estático de aceleración es aproximadamente infinito")
+            else:
+                if Ka< 0.01:
+                    st.write("La constante de error estático de aceleración es aproximadamente 0")
+                else:
+                    st.write(f"la constante de error estática de aceleración es {round(Kp, 3)}")
+
+            
